418e94823935c6d78b8f8f762e28f418ba48a75b
Sal <desapi75@students.rowan.edu>
2015-04-21 22:31:37 -0400

    added ssh_command

---
+++ ssh_command.pl
@@ -0,0 +1,13 @@
+% remotely log into a unix system
+
+:- multifile verb/3, noun/3.
+
+verb(ssh) --> [login].
+verb(ssh) --> [ssh].
+verb(ssh) --> [access].
+
+ssh(Noun) :-
+        string_concat('ssh ', Noun, Out),
+        shell(Out).
+
+
